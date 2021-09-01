5f3c2ad68f4c5bc98898ea1574522eed4abe363a
masaki <keitomasaki@icloud.com>
2021-09-01 23:06:44 +0900

    s

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
   const facebookLogin = () => {
     // フェイスブックログイン処理
     const provider = new firebase.auth.FacebookAuthProvider();
-    // provider.addScope("instagram_graph_user_profile");
-    // provider.addScope("user_birthday");
-    provider.addScope("user_friends");
+
     provider.addScope("pages_show_list");
     provider.addScope("instagram_basic");
     firebase
